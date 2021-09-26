bfff856a83e0dae699a0bd0fab65488538c7349a
flaxyars <74737840+emreunal2@users.noreply.github.com>
2021-09-26 18:25:22 +0300

    init

--- test.cs
+++ test.cs
@@ -6,8 +6,13 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
+      string myString ="15";
       Console.WriteLine("Hello World!");    
-      Console.WriteLine("Test");    
+      Console.WriteLine("Test");
+      Console.WriteLine(myString);
+      Console.WriteLine(Convert.ToInt32(myString));
+
+   
     }
   }
 }
